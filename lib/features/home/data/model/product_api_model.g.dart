883d49bdfdf2f39ac3cbc17936758dc89b2ca0a3
“Dinesh98490” <kunwardinesh028@gmail.com>
2025-07-15 13:38:03 +0545

    feat: add product_api_model.g.dart

---
+++ lib/features/home/data/model/product_api_model.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_api_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ProductApiModel _$ProductApiModelFromJson(Map<String, dynamic> json) =>
+    ProductApiModel(
+      id: json['_id'] as String?,
+      title: json['title'] as String,
+      desc: json['desc'] as String,
+      price: (json['price'] as num).toDouble(),
+      image: json['image'] as String,
+      color: json['color'] as String,
+      size: json['size'] as String,
+      quantity: (json['quantity'] as num).toInt(),
+      category:
+          CategoryApiModel.fromJson(json['categoryId'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$ProductApiModelToJson(ProductApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'title': instance.title,
+      'desc': instance.desc,
+      'price': instance.price,
+      'image': instance.image,
+      'color': instance.color,
+      'size': instance.size,
+      'quantity': instance.quantity,
+      'categoryId': instance.category,
+    };
